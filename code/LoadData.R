4dbe49ff9669de649952bff0d77dee619477c787
Desmond Cole <desmond.r.cole@gmail.com>
2018-04-12 22:30:43 -0400

    Update data loading with county-level population.

--- code/LoadData.R
+++ code/LoadData.R
@@ -2,20 +2,13 @@
 library(plyr)
 library(dplyr)
 library(data.table)
+library(magrittr)
 
 #This Pulls the initial csv files exported from Kaggle. 
 #Probably unnecessary to include the root code used to generate the csv files themselves.
 setwd("/Users/Desmond/Desktop/Work/503 Project")
 datasetnames = c("accident.csv",
                  "cevent.csv",
-                 "damage.csv",
-                 "distract.csv",
-                 "drimpair.csv",
-                 "factor.csv",
-                 "maneuver.csv",
-                 "nmcrash.csv",
-                 "nmimpair.csv",
-                 "nmprior.csv",
                  "parkwork.csv",
                  "pbtype.csv",
                  "person.csv",
@@ -25,7 +18,15 @@ datasetnames = c("accident.csv",
                  "vindecode.csv",
                  "violatn.csv",
                  "vision.csv",
-                 "vsoe.csv")
+                 "vsoe.csv",
+                 "damage.csv",
+                 "distract.csv",
+                 "drimpair.csv",
+                 "factor.csv",
+                 "maneuver.csv",
+                 "nmcrash.csv",
+                 "nmimpair.csv",
+                 "nmprior.csv")
 
 #Pull 2015 data
 setwd("./2015")
@@ -45,6 +46,18 @@ for(i in 1:20){
   allfiles[[i]] = rbind(files_2015[[i]],files_2016[[i]])
 }
 names(allfiles) = datasetnames
+
+#Load county-level population
+allfiles[[1]] = allfiles[[1]] %>%
+  .[,`:=` (FIPS = state_number*1000 + county),]
+
+censusdata = fread("/Users/Desmond/Desktop/Work/503 Project/cc-est2016-alldata.csv") %>% 
+  .[,`:=` (FIPS = as.factor(1000*STATE + COUNTY))] %>%
+  .[,`:=` (Year = 2015 * (YEAR==8) + 2016*(YEAR==9))] %>%
+  .[((Year==2015 | Year==2016) & AGEGRP == 0),.(Population=as.numeric(TOT_POP)),by=.(FIPS,Year)]
+
+allfiles[[1]]  = join(allfiles[[1]],censusdata,by = c("Year","FIPS"),type="left")
+
 
 setwd("/Users/Desmond/Desktop/Work/503 Project/AllData")
 for(i in 1:length(allfiles)){
