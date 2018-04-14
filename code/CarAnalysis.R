43d92914481af6ff75327bc6ac68ab605f01d82a
Desmond Cole <desmond.r.cole@gmail.com>
2018-04-14 16:25:02 -0400

    Update car/classification code

--- code/CarAnalysis.R
+++ code/CarAnalysis.R
@@ -11,18 +11,8 @@ names(allfiles) = temp
 
 setwd("/Users/Desmond/Documents/GitHub/503Project")
 
-#Aggregate by make
-MakeData = allfiles[[15]][!(vehicle_configuration_name %in% 
-                              c("Truck Tractor/Semi-Trailer",
-                                "Truck Tractor (Bobtail, i.e., Tractor Only, No Trailer)",
-                                "Truck Tractor/Double",
-                                "Truck Tractor/Triple",
-                                "Truck More than 10,000 lbs., Cannot Classify",
-                                "Bus (Seats for More Than 15 Occupants, Including Driver, 2010-Later",
-                                "Single-Unit Truck (3 or More axles)",
-                                "Single-Unit Truck (2 axles and GVWR more\nthan 10,000 lbs."))
-                          ,.(fatalaccidents = .N),by=vehicle_make_name]
 
+#Generate Maker Data
 Makers = c("GM","Ford","Toyota",
            "FCA/Chrysler","Honda",
            "Nissan","Hyundai-Kia","Subaru",
@@ -47,46 +37,79 @@ RelMarketNumbers = c(45276000,
 
 MakerData = data.table(Maker = seq(1:15),Makers = Makers,MarketNumbers = RelMarketNumbers)
 
-MakeData = MakeData[,`:=` (Maker = 1*(vehicle_make_name %in% c("Chevrolet","Saturn","Pontiac","Oldsmobile","Saab","Buick/Opel","Cadillac","GMC")) + 
-                             2*(vehicle_make_name %in% c("Ford","Lincoln","Mercury")) + 
-                             3*(vehicle_make_name %in% c("Toyota","Daihatsu","Lexus","Scion (Since 2012)")) + 
-                             4*(vehicle_make_name %in% c("Fiat","Chrysler","Dodge","Jeep/Kaiser-Jeep/Willys Jeep","Plymouth")) + 
-                             5*(vehicle_make_name %in% c("Datsun/Nissan","Infiniti","Bluebird")) + 
-                             6*(vehicle_make_name %in% c("Honda","Acura")) + 
-                             7*(vehicle_make_name %in% c("Hyundai","Kia")) + 
-                             8*(vehicle_make_name %in% c("Subaru")) + 
-                             9*(vehicle_make_name %in% c("Audi","Bentley","Porsche","Ducati","Volkswagen")) + 
-                             10*(vehicle_make_name %in% c("Mercedes-Benz","Smart (Since 2010)")) + 
-                             11*(vehicle_make_name %in% c("BMW")) + 
-                             12*(vehicle_make_name %in% c("Mazda")) + 
-                             13*(vehicle_make_name %in% c("Mitsubishi")) + 
-                             14*(vehicle_make_name %in% c("Volvo")) + 
-                             15*(vehicle_make_name %in% c("Land Rover","Jaguar")))] %>%
-  .[Maker != 0, .(accidents = sum(fatalaccidents)),by=Maker]
-
-MakeData = MakeData[MakerData,on="Maker"] %>%
-  .[,`:=` (shareaccidents = 1000000*accidents/MarketNumbers)] %>%
-  .[order(-shareaccidents)]
 
 
-CarPlot = ggplot(MakeData,aes(x=reorder(Makers,-shareaccidents))) +
-  geom_bar(aes(weight=shareaccidents),fill="dark green") + 
-  labs(y = "Fatal Accidents per Million Cars",x = "Manufacturer") + 
+#Generate Vehicle data
+keepVars = c("uniqueID","number_of_drunk_drivers","police_reported_drug_involvement","driver_distracted_by_name",
+             "impairment_at_time_of_crash_driver_name","previous_recorded_crashes","previous_recorded_suspensions_and_revocations",
+             "previous_dwi_convictions","restraint_system_helmet_use_name","fire_occurrence","number_of_fatalities",
+             "hit_and_run","travel_speed","speed_limit","speeding_related")
+
+
+ClassificationData = allfiles[[1]] %>%
+  .[,intersect(keepVars,names(allfiles[[1]])),with=FALSE]
+
+for (i in c(4,5,13)){
+  tokeep = intersect(keepVars,names(allfiles[[i]]))
+  temp = allfiles[[i]][,tokeep,with=FALSE]
+  ClassificationData = ClassificationData[temp,on="uniqueID",allow.cartesian=TRUE]
+}
+
+VehicleData = allfiles[[15]][!(vehicle_configuration_name %in% 
+                                 c("Truck Tractor/Semi-Trailer",
+                                   "Truck Tractor (Bobtail, i.e., Tractor Only, No Trailer)",
+                                   "Truck Tractor/Double",
+                                   "Truck Tractor/Triple",
+                                   "Truck More than 10,000 lbs., Cannot Classify",
+                                   "Bus (Seats for More Than 15 Occupants, Including Driver, 2010-Later",
+                                   "Single-Unit Truck (3 or More axles)",
+                                   "Single-Unit Truck (2 axles and GVWR more\nthan 10,000 lbs."))] %>%
+  .[(previous_dwi_convictions < 99 & previous_recorded_crashes < 99 & previous_recorded_suspensions_and_revocations < 99),] %>%
+  .[(travel_speed < 300 & speed_limit < 98),] %>%
+  .[,`:=` (Maker = 1*(vehicle_make_name %in% c("Chevrolet","Saturn","Pontiac","Oldsmobile","Saab","Buick/Opel","Cadillac","GMC")) + 
+             2*(vehicle_make_name %in% c("Ford","Lincoln","Mercury")) + 
+             3*(vehicle_make_name %in% c("Toyota","Daihatsu","Lexus","Scion (Since 2012)")) + 
+             4*(vehicle_make_name %in% c("Fiat","Chrysler","Dodge","Jeep/Kaiser-Jeep/Willys Jeep","Plymouth")) + 
+             5*(vehicle_make_name %in% c("Datsun/Nissan","Infiniti","Bluebird")) + 
+             6*(vehicle_make_name %in% c("Honda","Acura")) + 
+             7*(vehicle_make_name %in% c("Hyundai","Kia")) + 
+             8*(vehicle_make_name %in% c("Subaru")) + 
+             9*(vehicle_make_name %in% c("Audi","Bentley","Porsche","Ducati","Volkswagen")) + 
+             10*(vehicle_make_name %in% c("Mercedes-Benz","Smart (Since 2010)")) + 
+             11*(vehicle_make_name %in% c("BMW")) + 
+             12*(vehicle_make_name %in% c("Mazda")) + 
+             13*(vehicle_make_name %in% c("Mitsubishi")) + 
+             14*(vehicle_make_name %in% c("Volvo")) + 
+             15*(vehicle_make_name %in% c("Land Rover","Jaguar")))] %>%
+  .[Maker != 0,] %>%
+  .[,c(intersect(keepVars,names(allfiles[[15]])),"Maker"),with=FALSE] %>%
+  .[MakerData,on="Maker"]
+
+ClassificationData = unique(ClassificationData)
+
+ClassificationData = ClassificationData[VehicleData,on="uniqueID"]
+
+SummaryData = ClassificationData[,.(deathspermillion = 1000000*sum(number_of_fatalities)/MarketNumbers),
+                      by=.(Makers,MarketNumbers)] %>%
+  .[order(-deathspermillion)]
+
+
+CarPlot = ggplot(SummaryData,aes(x=reorder(Makers,-deathspermillion))) +
+  geom_bar(aes(weight=deathspermillion),fill="dark green") + 
+  labs(y = "Fatalities per Million Cars",x = "Manufacturer") + 
   ggtitle("Ranking of Manufacturer by Fatal Accidents")
 CarPlot
 ggsave("./report/ManufacturerRankingPlot",plot=CarPlot,device="png",width=15.2,height=7.69,units="in")
 
 
 
-#Numerical Analysis. To whether there are differences among car manufacturers w/r/t certain behavioral characteristics, it is first necessary to 
-#appropriately sample from the data based on the frequency with which automakers appear. 
-
-#Generate automaker-specific dataset with relevant indicators. 
-
-#This portion is time-permitting. multiclass imbalance is tricker to address than binary class imbalance.
 
 
-library(caret)
+#Numerical Analysis. Even with observed differences among car manufacturers, it is unclear 
+#whether or not unobservables account for the majority of the differences. To explore this further, 
+#will use multi-class boosting (to account for class imbalance) for classification.
+library(maboost)
+
 
 
 
